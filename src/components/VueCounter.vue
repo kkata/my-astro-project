ff0a3241c1b82310fba7ea8deb7197a14c198f92
kkata <14849535+kkata@users.noreply.github.com>
2021-10-20 13:12:50 +0900

    use vue and scoped css

---
+++ src/components/VueCounter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="counter">
+    <button @click="subtract()">-</button>
+    <pre>{{ count }}</pre>
+    <button @click="add()">+</button>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const count = ref(0);
+    const add = () => (count.value = count.value + 1);
+    const subtract = () => (count.value = count.value - 1);
+
+    return {
+      count,
+      add,
+      subtract,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.counter {
+  display: flex;
+  & > * {
+    margin: 1em;
+  }
+}
+</style>
