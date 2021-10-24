5685ea1b001cbeefa13647567eab99f2c5812f43
kkata <14849535+kkata@users.noreply.github.com>
2021-10-24 16:44:33 +0900

    VueのrefとonMountedで要素にアクセスする

---
+++ src/components/Child.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>child component</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/components/Demo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div ref="root">
+    <Child />
+    <p>Demo component</p>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+import Child from "./Child.vue";
+export default {
+  components: { Child },
+  setup() {
+    const root = ref(null);
+
+    onMounted(() => {
+      // the DOM element will be assigned to the ref after initial render
+      console.log("onMounted");
+      console.log(root.value); // this is your $el
+    });
+
+    return {
+      root,
+    };
+  },
+};
+</script>
